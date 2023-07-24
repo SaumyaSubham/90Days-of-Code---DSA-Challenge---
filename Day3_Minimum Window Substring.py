d8915d1b2506909cb0a03b2e3351cda3a6d1fc21
SaumyaSubham <sm025663@gmail.com>
2023-07-25 00:05:26 +0530

    Day_3

--- Day3_Minimum Window Substring.py
+++ Day3_Minimum Window Substring.py
@@ -1,3 +1,29 @@
+# Minimum Window Substring                                       Difficulty = Hard
+
+
+
+# Given two strings s and t of lengths m and n respectively, return the minimum window 
+# substring
+#  of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+# The testcases will be generated such that the answer is unique.
+
+ 
+
+# Example 1:
+
+# Input: s = "ADOBECODEBANC", t = "ABC"
+# Output: "BANC"
+# Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+
+
+
+
+
+
+
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "": return ""
