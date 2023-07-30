2cfe6af2d83ddf1fd9d99a466d04cf19d80b75e5
SaumyaSubham <sm025663@gmail.com>
2023-07-30 22:10:54 +0530

    Day_9

---
+++ Day9_Merge k Sorted Lists.py
@@ -0,0 +1,58 @@
+# Merge k Sorted Lists                           Difficulty = Hard
+
+
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+# Merge all the linked-lists into one sorted linked-list and return it.
+
+
+# Example 1:
+
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        
+        mid = len(lists) // 2
+        left = self.mergeKLists(lists[:mid])
+        right = self.mergeKLists(lists[mid:])
+        
+        return self.merge(left, right)
+    
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        
+        curr.next = l1 or l2
+        
+        return dummy.next
