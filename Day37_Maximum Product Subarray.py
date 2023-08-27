40c58ae38c23afa683630119059d4a2f7465bb98
SaumyaSubham <sm025663@gmail.com>
2023-08-27 23:11:33 +0530

    Day_37

---
+++ Day37_Maximum Product Subarray.py
@@ -0,0 +1,34 @@
+# Maximum Product Subarray                            Difficulty = Easy
+
+# Given an integer array nums, find a 
+# subarray that has the largest product, and return the product.
+
+# The test cases are generated so that the answer will fit in a 32-bit integer.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,3,-2,4]
+# Output: 6
+# Explanation: [2,3] has the largest product 6.
+# Example 2:
+
+# Input: nums = [-2,0,-1]
+# Output: 0
+# Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        curMax, curMin = 1, 1
+        res = nums[0]
+        
+        for n in nums:
+            vals = (n, n * curMax, n * curMin)
+            curMax, curMin = max(vals), min(vals)
+			
+            res = max(res, curMax)
+            
+        return res
+    
