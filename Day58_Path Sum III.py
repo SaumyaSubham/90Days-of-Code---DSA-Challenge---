dc680c1b91edc443e25325ad52d877e8aad64e02
SaumyaSubham <sm025663@gmail.com>
2023-09-19 01:48:59 +0530

    Day_58

---
+++ Day58_Path Sum III.py
@@ -0,0 +1,49 @@
+# Path Sum III                                           Difficulty = Medium
+
+# Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+# The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+ 
+
+# Example 1:
+
+
+# Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+# Output: 3
+# Explanation: The paths that sum to 8 are shown.
+
+# Example 2:
+
+# Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+# Output: 3
+
+
+class Solution(object):
+    def pathSum(self, root, target):
+        # define global result and path
+        self.result = 0
+        cache = {0:1}
+        
+        # recursive to get result
+        self.dfs(root, target, 0, cache)
+        
+        # return result
+        return self.result
+    
+    def dfs(self, root, target, currPathSum, cache):
+        # exit condition
+        if root is None:
+            return  
+        # calculate currPathSum and required oldPathSum
+        currPathSum += root.val
+        oldPathSum = currPathSum - target
+        # update result and cache
+        self.result += cache.get(oldPathSum, 0)
+        cache[currPathSum] = cache.get(currPathSum, 0) + 1
+        
+        # dfs breakdown
+        self.dfs(root.left, target, currPathSum, cache)
+        self.dfs(root.right, target, currPathSum, cache)
+        # when move to a different branch, the currPathSum is no longer available, hence remove one. 
+        cache[currPathSum] -= 1    
