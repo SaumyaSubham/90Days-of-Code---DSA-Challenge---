b4a73bbc1bae90cbed2387ba0648decf357fd1b6
SaumyaSubham <sm025663@gmail.com>
2023-09-26 00:48:40 +0530

    Day_64

---
+++ Day64_Design Add and Search Words Data Structure.py
@@ -0,0 +1,63 @@
+# Design Add and Search Words Data Structure                     Difficulty = Medium
+
+
+
+# Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+# Implement the WordDictionary class:
+
+# WordDictionary() Initializes the object.
+# void addWord(word) Adds word to the data structure, it can be matched later.
+# bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+ 
+
+# Example:
+
+# Input
+# ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+# [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+# Output
+# [null,null,null,null,false,true,true,true]
+
+# Explanation
+# WordDictionary wordDictionary = new WordDictionary();
+# wordDictionary.addWord("bad");
+# wordDictionary.addWord("dad");
+# wordDictionary.addWord("mad");
+# wordDictionary.search("pad"); // return False
+# wordDictionary.search("bad"); // return True
+# wordDictionary.search(".ad"); // return True
+# wordDictionary.search("b.."); // return True
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.children = [None]*26
+        self.isCompleteWord = False
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self
+        for c in word:
+            if curr.children[ord(c) - ord('a')] == None:
+                curr.children[ord(c) - ord('a')] = WordDictionary()
+            curr = curr.children[ord(c) - ord('a')]
+        
+        curr.isCompleteWord = True
+        
+
+    def search(self, word: str) -> bool:
+        curr = self
+        for i in range(len(word)):
+            c = word[i]
+            if c == '.':
+                for ch in curr.children:
+                    if ch != None and ch.search(word[i+1:]): return True
+                return False
+            
+            if curr.children[ord(c) - ord('a')] == None: return False
+            curr = curr.children[ord(c) - ord('a')]
+        
+        return curr != None and curr.isCompleteWord
+    
