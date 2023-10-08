a9b8d4cfe19f163c1e1575a0d070165effcf90f4
SaumyaSubham <sm025663@gmail.com>
2023-10-09 01:12:18 +0530

    Day_76

---
+++ Day76_Floyd Warshall.py
@@ -0,0 +1,45 @@
+# Floyd Warshall                           Difficulty = Medium
+
+# The problem is to find the shortest distances between every pair of vertices in a given edge-weighted directed graph. The graph is represented as an adjacency matrix of size n*n. Matrix[i][j] denotes the weight of the edge from i to j. If Matrix[i][j]=-1, it means there is no edge from i to j.
+# Do it in-place.
+
+# Example 1:
+
+# Input: matrix = {{0,25},{-1,0}}
+
+# Output: {{0,25},{-1,0}}
+
+# Explanation: The shortest distance between
+# every pair is already given(if it exists).
+# Example 2:
+
+# Input: matrix = {{0,1,43},{1,0,6},{-1,-1,0}}
+
+# Output: {{0,1,7},{1,0,6},{-1,-1,0}}
+
+# Explanation: We can reach 2 from 0 as 0->1->2
+# and the cost will be 1+6=7 which is less than 
+# 43.
+
+class Solution:
+	def shortest_distance(self, matrix):
+		#Code here
+		n = len(matrix)
+        for i in range(n):
+            for j in range(n):
+                if matrix[i][j] == -1:
+                    matrix[i][j] = float("inf")
+                if(i == j):
+                    matrix[i][j] = 0
+   
+   # algorithm starts here 
+        for k in range(n):
+           for i in range(n):
+               for j in range(n):
+                    matrix[i][j] = min(matrix[i][j],(matrix[i][k]+matrix[k][j]))
+    
+        for i in range(n):
+           for j in range(n):
+               if matrix[i][j] == float("inf"):
+                    matrix[i][j] = -1
+        return matrix
