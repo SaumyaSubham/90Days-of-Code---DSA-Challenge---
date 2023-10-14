69fd16d1769d42a50fb255411b7ce342268eb9e0
SaumyaSubham <sm025663@gmail.com>
2023-10-14 21:40:44 +0530

    Day_80

---
+++ Day80_Range Sum Query 2D - Immutable.py
@@ -0,0 +1,45 @@
+# Range Sum Query 2D - Immutable                            Difficulty = Medium
+
+
+# Given a 2D matrix matrix, handle multiple queries of the following type:
+
+# Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+# Implement the NumMatrix class:
+
+# NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+# int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+# You must design an algorithm where sumRegion works on O(1) time complexity.
+
+ 
+
+# Example 1:
+
+
+# Input
+# ["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
+# [[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
+# Output
+# [null, 8, 11, 12]
+
+# Explanation
+# NumMatrix numMatrix = new NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]);
+# numMatrix.sumRegion(2, 1, 4, 3); // return 8 (i.e sum of the red rectangle)
+# numMatrix.sumRegion(1, 1, 2, 2); // return 11 (i.e sum of the green rectangle)
+# numMatrix.sumRegion(1, 2, 2, 4); // return 12 (i.e sum of the blue rectangle)
+
+
+
+
+class NumMatrix:
+    def __init__(self, matrix: List[List[int]]):
+        self.dp=[[0] * (len(matrix[0])+1) for _ in range(len(matrix)+1)]
+        
+		# calculate prefix sum
+        for r in range(len(self.dp)-1):
+            for c in range(len(self.dp[0])-1):
+                self.dp[r+1][c+1]=matrix[r][c] + self.dp[r][c+1] + self.dp[r+1][c] - self.dp[r][c]
+        
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.dp[row2+1][col2+1] - self.dp[row1][col2+1] - self.dp[row2+1][col1] + self.dp[row1][col1]
+    
+    
